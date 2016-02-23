2a9120eb5d3e4b1722edab9dcca2d9b29fab5397
ProductiveRage <dangger36@gmail.com>
2016-02-23 21:34:07 +0000

    Added validation to the MessageEditor component

---
+++ BridgeReactTutorial/Components/ValidatedTextInput.cs
@@ -0,0 +1,28 @@
+﻿using Bridge.React;
+
+namespace BridgeReactTutorial.Components
+{
+	public class ValidatedTextInput : StatelessComponent<ValidatedTextInput.Props>
+	{
+		public ValidatedTextInput(Props props) : base(props) { }
+
+		public override ReactElement Render()
+		{
+			var className = props.ClassName;
+			if (!string.IsNullOrWhiteSpace(props.ValidationMessage))
+				className = (className + " invalid").Trim();
+
+			return DOM.Span(new Attributes { ClassName = className },
+				new TextInput(props),
+				!string.IsNullOrWhiteSpace(props.ValidationMessage)
+					? DOM.Span(new Attributes { ClassName = "validation-message" }, props.ValidationMessage)
+					: null
+			);
+		}
+
+		public class Props : TextInput.Props
+		{
+			public string ValidationMessage;
+		}
+	}
+}
