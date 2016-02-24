a8af8d48388e494e1d24627990dd14b18c39ae60
ProductiveRage <742348+ProductiveRage@users.noreply.github.com>
2016-02-24 23:22:43 +0000

    Added a comment to the StoreInitialised action, because it's not the most obvious one in a simple application like this (its use is also documented where it's referenced in the code)

--- BridgeReactTutorial/Actions/StoreInitialised.cs
+++ BridgeReactTutorial/Actions/StoreInitialised.cs
@@ -2,6 +2,10 @@
 
 namespace BridgeReactTutorial.Actions
 {
+	/// <summary>
+	/// This action is raised when the app is ready, when the Dispatcher has been created and the initial Store is ready to be fired up (in a more
+	/// complex app
+	/// </summary>
 	public class StoreInitialised : IDispatcherAction
 	{
 		public object Store;
